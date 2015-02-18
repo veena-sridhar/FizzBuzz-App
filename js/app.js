a56a3763e9bc1317ab74f17012f565295aa481d1
veena-sridhar <v.sridhar@gmail.com>
2015-02-18 11:15:06 -0800

    Final JavaScript for FizzBuzz added

--- js/app.js
+++ js/app.js
@@ -1,2 +1,14 @@
+for (var i = 1; i < 101; i++) {
+	document.body.innerHTML += " <br /> "
+	if ( i % 3 === 0 && i % 5 === 0) {
+		document.write('fizzbuzz');
+	} else if ( i % 3 === 0) {
+		document.write('fizz');
+	} else if ( i % 5 === 0) {
+		document.write('buzz');
+	} else {
+		document.write(i);
+	}	
+} 
 
 
